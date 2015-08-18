78c7fe652222a0bf74483305fff77959c95dd5e7
Thibaud Fabre <thibaud@aztech.io>
2015-08-18 11:03:10 +0200

    Complete bad result test

--- tests/unit/Bundle/EventListener/BadRequestListenerTest.php
+++ tests/unit/Bundle/EventListener/BadRequestListenerTest.php
@@ -3,14 +3,12 @@
 namespace Alchemy\RestBundle\Tests\EventListener;
 
 use Alchemy\Rest\Result\BadRequestResult;
+use Alchemy\Rest\Result\SuccessResult;
 use Alchemy\RestBundle\EventListener\BadRequestListener;
 use Symfony\Component\HttpFoundation\Response;
 
 class BadRequestListenerTest extends GetControllerResultTest
 {
-
-
-
     public function testBadRequestResultsAreConvertedToHttpResponses()
     {
         $event = $this->getControllerResultEvent(new BadRequestResult());
@@ -23,5 +21,15 @@ class BadRequestListenerTest extends GetControllerResultTest
 
         $this->assertInstanceOf('Symfony\Component\HttpFoundation\Response', $result);
         $this->assertEquals(400, $result->getStatusCode());
+    }
+
+    public function testOnlyBadRequestResultsAreTransformed()
+    {
+        $event = $this->getControllerResultEvent(new SuccessResult());
+        $listener = new BadRequestListener();
+
+        $listener->onKernelView($event);
+
+        $this->assertInstanceOf('Alchemy\Rest\Result\SuccessResult', $event->getControllerResult());
     }
 }
